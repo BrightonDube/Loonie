c2806f73542a2a78ea880e39d7b0bdc395dccfdf
Brighton Dube <BrightonDube@users.noreply.github.com>
2018-08-20 22:58:13 +0200

    Delete helloserver.js

--- helloserver.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function (request, response) {
-    // Send the HTTP OK Header
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // Send the response body
-    response.end('Hello World\n');
-}).listen(8081);
-console.log("Server running at http://localhost:8081");
